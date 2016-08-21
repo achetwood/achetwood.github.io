587ecd69fd15bb5544a2019120e3ffd736782e91
Alex Chetwood <achetwood@hotmail.co.uk>
2016-08-21 19:57:10 +0100

    Changes

--- teststuff/Zegami.js
+++ teststuff/Zegami.js
@@ -33,18 +33,19 @@ function Zegami() {
 		// Initialise PIXI stage and renderer.
 		// Attach renderer to the intended element.
 		var canvas = document.getElementById(container);
-		var wWidth = window.innerWidth; // document.body.clientWidth;
-		var wHeight = window.innerHeight; // document.body.clientHeight;
+		var wWidth = window.innerWidth;
+		var wHeight = window.innerHeight; 
 		_renderer = PIXI.autoDetectRenderer(wWidth, wHeight, {backgroundColor : 0xFFFFFF});
 		canvas.appendChild(_renderer.view);
 		
 		_stage = new PIXI.Container();
 		
-		_stage.x = 60;
-		_stage.y = 60;
+		_stage.x = 10;
+		_stage.y = 10;
 		console.log(_stage.scale.x);
 		
-		// var containerDiv = document.createElement('div');
+		var containerDiv = document.createElement('div');
+		
 		var zoomSlider = document.createElement('input');
 		zoomSlider.id = "zoomControl";
 		zoomSlider.type = 'range';
@@ -57,11 +58,11 @@ function Zegami() {
 			_stage.scale.x = val;
 			_stage.scale.y = val;}
 		);
-		document.body.appendChild(zoomSlider);
+		containerDiv.appendChild(zoomSlider);
 		
-		var selectedDiv = document.createElement('div');
-		selectedDiv.id = "picSelected";
-		selectedDiv.innerHTML = "Selected: ";
+		var selectedDiv = document.createElement('span');
+		selectedSpan.id = "picSelected";
+		selectedSpan.innerHTML = "Selected: ";
 		
 		// var selectedBox = document.createElement('input');
 		// selectedBox.id = "picSelected";
@@ -69,8 +70,8 @@ function Zegami() {
 		// selectedBox.readonly = true;
 		
 		// selectedDiv.appendChild(selectedBox);
-		document.body.appendChild(selectedDiv);
-		
+		containerDiv.appendChild(selectedSpan);
+		document.body.appendChild(containerDiv);
 		// this.start = function(img) {
 			// var numImgX = _numImgX;
 			// var numImgY = _numImgY;
@@ -126,284 +127,60 @@ function Zegami() {
 	}
 	
 	this.start = function(img) {
-			var numImgX = _numImgX;
-			var numImgY = _numImgY;
-			var renderer = _renderer;
-			var stage = _stage;
-			var spriteArr = _spriteArr;
+		
+		// Create function scope copies of the private vars.
+		var numImgX = _numImgX;
+		var numImgY = _numImgY;
+		var renderer = _renderer;
+		var stage = _stage;
+		var spriteArr = _spriteArr;
+		
+		var image = new Image();
+		
+		image.onload = function() {
+			// Create base texture and rexture from the loaded image.
+			var baseTexture = new PIXI.BaseTexture(image)
+			var texture = new PIXI.Texture(baseTexture);
 			
-			var image = new Image();
-			
-			image.onload = function() {
-				var baseTexture = new PIXI.BaseTexture(image)
-				var texture = new PIXI.Texture(baseTexture);
-				
-				// function populateImages()
-				// {
-					// Set variable ready for loop when adding images.
-					// var x = 0;
-					// var y = 0;
-					for ( y = 0; y < numImgY; y++ )
-					{
-						for ( x = 0; x < numImgX; x++)
-						{
-							var imgSprite = new PIXI.Sprite(texture);
-							imgSprite.x = (texture.width + 20) * x;
-							imgSprite.y = (texture.height + 20) * y;
-							
-							imgSprite.interactive = true;
-							
-							// Add images to array and bind mouse click/touch event to sprites.
-							spriteArr.push(imgSprite);
-							var currLength = spriteArr.length - 1;
-							spriteArr[currLength].on('mousedown', onClick.bind(currLength));
-							// spriteArr[currLength].on('touchstart', onClick(currLength + 1));
-							
-							function onClick()
-							{
-								var imgNum = this + 1;
-								var txtBox = document.getElementById('picSelected');
-								txtBox.innerHTML = "Selected: " + imgNum;
-								
-								//this.scale.x += 0.3;
-								//this.scale.y += 0.3;
-							}
-							
-							stage.addChild(imgSprite);
-						}
-					}
+			for ( y = 0; y < numImgY; y++ )
+			{
+				for ( x = 0; x < numImgX; x++)
+				{
+					var imgSprite = new PIXI.Sprite(texture);
+					imgSprite.x = (texture.width + 20) * x;
+					imgSprite.y = (texture.height + 20) * y;
 					
-					animate();
-					// animate();
-				// }
-			};
+					imgSprite.interactive = true;
+					
+					// Add images to array and bind mouse click/touch event to sprites.
+					spriteArr.push(imgSprite);
+					// Get the current sprite in the sprite array and add event listener to that sprite.
+					// Using bind to bind the array position data to the function, allows the population of the text box.
+					var currLength = spriteArr.length - 1;
+					spriteArr[currLength].on('mousedown', onClick.bind(currLength));
+					spriteArr[currLength].on('touchstart', onClick.bind(currLength));
+					
+					function onClick()
+					{
+						var imgNum = this + 1;
+						var txtBox = document.getElementById('picSelected');
+						txtBox.innerHTML = "Selected: " + imgNum;
+					}
+					// Add sprite to the stage.
+					stage.addChild(imgSprite);
+				}
+			}
 			
-			image.src = img;
-			// animate();
-		}
-	// this.size = function(numX, numY) {
-		// _numImgX = numX;
-		// _numImgY = numY;
-	// }
-	
-	// this.start = function(img) {
-		// var numImgX = _numImgX;
-		// var numImgY = _numImgY;
-		// var spriteArr = _spriteArr;
-		// // var renderer = this.renderer;
-		// var stage = _stage;
-		
-		// var image = new Image();
-		
-		// image.onload = function() {
-			// var baseTexture = new PIXI.BaseTexture(image)
-			// var texture = new PIXI.Texture(baseTexture);
-			
-			// // function populateImages()
-			// // {
-				// // Set variable ready for loop when adding images.
-				// // var x = 0;
-				// // var y = 0;
-				// for ( y = 0; y < numImgY; y++ )
-				// {
-					// for ( x = 0; x < numImgX; x++)
-					// {
-						// var imgSprite = new PIXI.Sprite(texture);
-						// imgSprite.x = (texture.width * x) + 50;
-						// imgSprite.y = (texture.height * y) + 50;
-						
-						// imgSprite.interactive = true;
-						
-						// // Add images to array and bind mouse click/touch event to sprites.
-						// spriteArr.push(imgSprite);
-						// spriteArr[spriteArr.length - 1].on('mousedown', onClick);
-						// spriteArr[spriteArr.length - 1].on('touchstart', onClick);
-						
-						// function onClick()
-						// {
-							// this.scale.x += 0.3;
-							// this.scale.y += 0.3;
-						// }
-						
-						// stage.addChild(imgSprite);
-					// }
-				// }
-				
-			// // }
-		// };
-		
-		// image.src = img;
-		// animate();
-	// }
+			animate();
+		};
+		// Set image source location to the location passed into start().
+		image.src = img;
+	}
 	
 	function animate() {
 		requestAnimationFrame(animate);
 		
 		_renderer.render(_stage);
-	}
-}
-
-// Zegami.prototype.constructor = Zegami;
-// Zegami.prototype.attachTo = function(container) {
-	// // Initialise PIXI stage and renderer.
-	// // Attach renderer to the intended element.
-	// var canvas = document.getElementById(container);
-	
-	// this.renderer = PIXI.autoDetectRenderer(800, 800,{backgroundColor : 0xFFFFFF});
-	// canvas.appendChild(this.renderer.view);
-	
-	// this.stage = new PIXI.Container();
-	
-	// this.stage.x = 60;
-	// this.stage.y = 60;
-	
-	// var zoomSlider = document.createElement('input');
-	// zoomSlider.id = "zoomControl";
-	// zoomSlider.type = 'range';
-	// zoomSlider.min = 5;
-	// zoomSlider.max = 300;
-	// zoomSlider.value = 1;
-	// zoomSlider.step = 1;
-	// document.body.appendChild(zoomSlider);
-	
-	// // this.size = function(numX, numY) {
-		// // this.numImgX = numX;
-		// // this.numImgY = numY;
-	// // }
-	
-	// // this.start = function(img) {
-		// // var numImgX = this.numImgX;
-		// // var numImgY = this.numImgY;
-		// // var renderer = this.renderer;
-		// // var stage = this.stage;
-		
-		// // var image = new Image();
-		
-		// // image.onload = function() {
-			// // var baseTexture = new PIXI.BaseTexture(image)
-			// // var texture = new PIXI.Texture(baseTexture);
-			
-			// // // function populateImages()
-			// // // {
-				// // // Set variable ready for loop when adding images.
-				// // // var x = 0;
-				// // // var y = 0;
-				// // for ( y = 0; y < numImgY; y++ )
-				// // {
-					// // for ( x = 0; x < numImgX; x++)
-					// // {
-						// // var imgSprite = new PIXI.Sprite(texture);
-						// // imgSprite.x = (texture.width * x) + 50;
-						// // imgSprite.y = (texture.height * y) + 50;
-						
-						// // imgSprite.interactive = true;
-						
-						// // // Add images to array and bind mouse click/touch event to sprites.
-						// // Zegami.spriteArr.push(imgSprite);
-						// // Zegami.spriteArr[Zegami.spriteArr.length - 1].on('mousedown', onClick);
-						// // Zegami.spriteArr[Zegami.spriteArr.length - 1].on('touchstart', onClick);
-						
-						// // function onClick()
-						// // {
-							// // this.scale.x += 0.3;
-							// // this.scale.y += 0.3;
-						// // }
-						
-						// // Zegami.stage.addChild(imgSprite);
-					// // }
-				// // }
-				
-				// // animate();
-			// // // }
-		// // };
-		
-		// // image.src = img;
-		
-		// // function animate() {
-			// // requestAnimationFrame(animate);
-			
-			// // renderer.render(stage);
-		// // }
-	// // }
-	// // var selectedImg = document.createElement();
-// }
-// Zegami.prototype.size = function(numX, numY) {
-	// this.numImgX = numX;
-	// this.numImgY = numY;
-// }
-// Zegami.prototype.start = function(img) {
-	// // Initialise	
-	// var numImgX = this.numImgX;
-	// var numImgY = this.numImgY;
-	// var renderer = this.renderer;
-	// var stage = this.stage;
-	
-	// var image = new Image();
-	
-	// image.onload = function() {
-		// var baseTexture = new PIXI.BaseTexture(image)
-		// var texture = new PIXI.Texture(baseTexture);
-		
-		// // function populateImages()
-		// // {
-			// // Set variable ready for loop when adding images.
-			// // var x = 0;
-			// // var y = 0;
-			// for ( y = 0; y < numImgY; y++ )
-			// {
-				// for ( x = 0; x < numImgX; x++)
-				// {
-					// var imgSprite = new PIXI.Sprite(texture);
-					// imgSprite.x = (texture.width * x) + 50;
-					// imgSprite.y = (texture.height * y) + 50;
-					
-					// imgSprite.interactive = true;
-					
-					// // Add images to array and bind mouse click/touch event to sprites.
-					// Zegami.spriteArr.push(imgSprite);
-					// Zegami.spriteArr[Zegami.spriteArr.length - 1].on('mousedown', onClick);
-					// Zegami.spriteArr[Zegami.spriteArr.length - 1].on('touchstart', onClick);
-					
-					// function onClick()
-					// {
-						// this.scale.x += 0.3;
-						// this.scale.y += 0.3;
-					// }
-					
-					// Zegami.stage.addChild(imgSprite);
-				// }
-			// }
-			
-			// animate();
-		// // }
-	// };
-	
-	// image.src = img;
-	
-	// function animate() {
-		// requestAnimationFrame(animate);
-		
-		// renderer.render(stage);
-	// }
-// }
-
-//Function to pre-load images from Konva docs
-function loadImages(sources, callback) {
-	var images = {};
-	var loadedImages = 0;
-	var numImages = 0;
-	// get num of sources
-	for(var src0 in sources) {
-		numImages++;
-	}
-	for(var src in sources) {
-		images[src] = new Image();
-		images[src].onload = function() {
-			if(++loadedImages >= numImages) {
-				callback(images);
-			}
-		};
-		images[src].src = sources[src];
 	}
 }
 
